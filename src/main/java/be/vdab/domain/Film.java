eb40771f5e989674281ddfe181bcd31be4bf6669
Lars De Pauw <depauwlars@gmail.com>
2015-07-10 14:41:53 +0200

    changes to class Film

---
+++ src/main/java/be/vdab/domain/Film.java
@@ -0,0 +1,14 @@
+package be.vdab.domain;
+
+public class Film {
+    private String title;
+    private Cast cast;
+    private int length;
+    private Director director;
+    private String summary;
+    private Genre genre;
+    private double rating;
+    private byte[] image;
+    private String trailerUrl;
+
+}
